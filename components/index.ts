fc9ff29aebc5ab91d4b611d02c7cf8d8a9880b81
UmerGillani36 <syedomershahgillani@gmail.com>
2023-07-07 19:02:40 +0500

    importing comp

--- components/index.ts
+++ components/index.ts
@@ -5,6 +5,7 @@ import Footer from "./Footer";
 import CustomFilter from "./CustomFilter";
 import SearchBar from "./SearchBar";
 import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
 export {
     Hero,
     CustomButton,
@@ -12,5 +13,6 @@ export {
     Footer,
     CustomFilter,
     SearchBar,
-    SearchManufacturer
+    SearchManufacturer,
+    CarCard
 }
