7faad00069b1f1b2097b94e6f1895d23505af5f9
UmerGillani36 <syedomershahgillani@gmail.com>
2023-07-07 15:15:50 +0500

    types

--- types/index.ts
+++ types/index.ts
@@ -3,5 +3,10 @@ import { MouseEventHandler } from "react";
 export interface CustomButtonProps {
     title: string,
     containerStyles?:string,
-    handleClick?:MouseEventHandler<HTMLButtonElement>
+    handleClick?:MouseEventHandler<HTMLButtonElement>,
+    btnType?: 'button' | 'submit' ;
+}
+export interface SearchManufacturer{
+    manufacturer:string,
+    setManufacturer:(manufacturer:string)=>void;
 }
