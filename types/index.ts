7b87eb0c6e3e63fa80cc6bce38981519217009d2
UmerGillani36 <syedomershahgillani@gmail.com>
2023-07-07 19:02:47 +0500

    types

--- types/index.ts
+++ types/index.ts
@@ -1,12 +1,30 @@
 import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
-    title: string,
-    containerStyles?:string,
-    handleClick?:MouseEventHandler<HTMLButtonElement>,
+    title: string;
+    containerStyles?:string;
+    handleClick?:MouseEventHandler<HTMLButtonElement>;
     btnType?: 'button' | 'submit' ;
+    textStyles?:string;
+    rightIcon?:string;
+    isDisabled?:boolean;
 }
 export interface SearchManufacturer{
-    manufacturer:string,
+    manufacturer:string;
     setManufacturer:(manufacturer:string)=>void;
+}
+
+export interface CarProps{
+    city_mpg:number;
+    class:string;
+    combination_mpg:number;
+    cylinders:number;
+    displacement:number;
+    drive:string;
+    fuel_type:string;
+    highway_mpg:number;
+    make:string;
+    model:string;
+    transmission:string;
+    year:number;
 }
