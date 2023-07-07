7d85ea6ac014fa3b84ddc8d4bf0beea22edc9300
UmerGillani36 <syedomershahgillani@gmail.com>
2023-07-07 19:02:16 +0500

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { fetchCars } from "@/utils";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+
+  const isEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   return (
     <main className="overflow-hidden">
       <Hero/>
@@ -16,6 +20,25 @@ export default function Home() {
             <CustomFilter title="year"/>
           </div>
         </div>
+
+        {
+          !isEmpty ?(
+            <section>
+              <div className="home__cars-wrapper">
+                {
+                  allCars.map((car)=>(
+                    <CarCard car={car}/>
+                  ))
+                }
+              </div>
+            </section>
+          ):(
+            <div className="home__error-container">
+              <h2 className="text-black text-xl font-bold">Oops, no result</h2>
+              <p>{allCars?.message}</p>
+            </div>
+          )
+        }
       </div>
     </main>
   )
