2d6d70ae8e60b6bf941ce391ea096d6d6fe2920f
UmerGillani36 <syedomershahgillani@gmail.com>
2023-07-07 15:14:59 +0500

    hero section fixed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,8 +21,8 @@ const Hero = () => {
         <div className='hero__image-container'>
             <div className='hero__image'>
                 <Image src="/hero.png" alt="heo" fill className='object-contain'/>
-                <div className='hero__image-overlay'/>
             </div>
+                <div className='hero__image-overlay'/>
         </div>
     </div>
   )
